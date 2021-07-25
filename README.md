7e193c64255b3b69f572d2d62f1f1908af7e440a
Anil <parshad.anil@gmail.com>
2021-07-25 16:23:34 -0500

    reduced data for faster training

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Takes in a handwritten equation and solves it.
 pip install patool (for extracting rar)
 pip install kaggle (download dataset)
 pip install opencv-python
+ - opencv_version = "4.5.3.56"
 pip install pillow (to read jpg file)
 instructions for setting up kaggle api used for downloading data set https://adityashrm21.github.io/Setting-Up-Kaggle/
 
@@ -14,3 +15,8 @@ kaggle datasets download -d xainano/handwrittenmathsymbols
 
 ### Notes
 runfile('C:/Users/{Username}/Documents/Udacity/NanoDegree/DataScience/Projects/Capstone/Handwritten-Equation-Solver/main.py', wdir='C:/Users/{Username}/Documents/Udacity/NanoDegree/DataScience/Projects/Capstone/Handwritten-Equation-Solver')
+
+### References
+https://www.pyimagesearch.com/2020/08/24/ocr-handwriting-recognition-with-opencv-keras-and-tensorflow/
+https://vipul-gupta73921.medium.com/handwritten-equation-solver-using-convolutional-neural-network-a44acc0bd9f8
+https://github.com/vipul79321/Handwritten-Equation-Solver
--- prep_data/extract_features.py
+++ prep_data/extract_features.py
@@ -1,12 +1,50 @@
 import cv2
 import matplotlib.pyplot as plt
 from constants.constants import ROOT_DIR
+
+
 def img_thing():
+    train_data = []
     # read the image file
     img = plt.imread(ROOT_DIR + '/resource/extracted_images/5/5_56.jpg')
 
     # invert image
     invert = cv2.bitwise_not(img)
+    # Sort contours
+    contours, hierarchy = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # sort contours form left to right
+    sorted_bounding_boxes = sort_contours(contours)
 
+    # resize bounding boxes to 28 x 28
+    maxi = 0
+    for box in sorted_bounding_boxes:
+        # get top right corner point as well as width and height of box
+        x, y, width, height = cv2.boundingRect(box)
+        maxi = max(width * height, maxi)
+        if maxi == width * height:
+            x_max = x
+            y_max = y
+            w_max = width
+            h_max = height
+
+        # crop the bounding box(the contour) on the original inverted image
+        crop_img = invert[y_max:y_max+h_max+10, x_max:x_max+w_max+10]
+        # resize the cropped image and scale to a 28 x 28 image
+        resize_img = cv2.resize(crop_img, (28, 28))
+        train_data.append(resize_img)
+
+
+    # used for verification
+    # cv2.imwrite('invert_pic.jpg', invert)
     # print(invert)
-    return invert
+
+    return train_data
+
+
+def sort_contours(ctrs):
+    # construct the array of bounding boxes and sort them from left to right
+
+    bounding_boxes = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+    # return the list of sorted contours and bounding boxes
+    return bounding_boxes
