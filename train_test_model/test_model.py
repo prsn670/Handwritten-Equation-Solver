3e2d75a3c3cb6adbc192ec6a7cd437841e67f8fb
Anil <parshad.anil@gmail.com>
2021-07-31 04:57:09 -0500

    remove unnecessary bool

--- train_test_model/test_model.py
+++ train_test_model/test_model.py
@@ -25,7 +25,6 @@ def test_model():
 
         result_string = ''
         for data in test_data:
-            eval_bool = False
             evaluate_data = np.array(data)
             evaluate_data = data.reshape(1, evaluate_data.shape[0], evaluate_data.shape[1], 1)
             result = loaded_model.predict_classes(evaluate_data)
@@ -35,10 +34,9 @@ def test_model():
             if result_string == test_label_dict[file_name]:
                 print('Match')
                 correct += 1
-                eval_bool = True
+                print(f'Resulting string evaluates to: {eval(result_string)}')
         print(f'model equation result: {result_string}')
         print(f'actual equation: {test_label_dict[file_name]}\n')
-        if eval_bool:
-            print(f'Resulting string evaluates to: {eval(result_string)}')
+
 
     print(f'Correct number of guesses: {correct} out of {len(os.listdir(TEST_DATA))}')
